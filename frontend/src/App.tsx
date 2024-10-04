c14ace7465bca285afb1da996b2c2a2d395788b3
stefansiebers <s3f3s-github@yahoo.com>
2024-10-04 12:32:23 +0200

    add axios connection and list all groceries

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
 import './App.css'
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {Grocery} from "./types/Grocery.ts";
 
 function App() {
-
+    const [groceries, setGroceries] = useState<Grocery[]>([])
+    const getAllGroceries = () => {
+        axios.get("/api/groceries")
+            .then(response => setGroceries(response.data))
+            .catch(error => console.error(error))
+    }
+    useEffect(() => getAllGroceries(), [])
+    console.log(groceries)
   return (
     <>
       <h1>Hello World</h1>
