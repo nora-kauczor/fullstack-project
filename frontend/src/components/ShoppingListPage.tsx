43da5c17031ea3c89442f6f0aacd6b4e856794d0
stefansiebers <s3f3s-github@yahoo.com>
2024-10-08 09:53:13 +0200

    calculate total amount of shopping list

--- frontend/src/components/ShoppingListPage.tsx
+++ frontend/src/components/ShoppingListPage.tsx
@@ -1,13 +1,20 @@
 import {Grocery} from "../types/Grocery.ts";
 import GroceryList from "./GroceryList.tsx";
+import {useEffect, useState} from "react";
 
 type Props = {
     groceries: Grocery[],
 }
 export default function ShoppingListPage(props: Readonly<Props>) {
-
+    const [total, setTotal] = useState<number>(0)
+    useEffect(() => {setTotal(calculateTotal())}, [props.groceries])
+    function calculateTotal():number{
+        return props.groceries.map(grocery => grocery.price * grocery.quantity)
+            .reduce((a,b) => a + b)
+    }
     return (<div id="all-products-page">
             <GroceryList groceries={props.groceries.filter(grocery => grocery.quantity > 0) }/>
+            <p>{total.toFixed(2)} €</p>
         </div>
             )
    }
