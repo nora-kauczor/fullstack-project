1cca56099fe360f16a248fea7107cd62204b3849
stefansiebers <s3f3s-github@yahoo.com>
2024-10-09 08:14:19 +0200

    simplify calculation of total price: change map+reduce to reduce only

--- frontend/src/components/ShoppingListPage.tsx
+++ frontend/src/components/ShoppingListPage.tsx
@@ -10,8 +10,7 @@ export default function ShoppingListPage(props: Readonly<Props>) {
     const [total, setTotal] = useState<number>(0)
     useEffect(() => {setTotal(calculateTotal())}, [props.groceries])
     function calculateTotal():number{
-        return props.groceries.map(grocery => grocery.price * grocery.quantity)
-            .reduce((a,b) => a + b, 0)
+        return props.groceries.reduce((sum, grocery) => sum + grocery.price * grocery.quantity, 0)
     }
     return (<div id="all-products-page">
             <GroceryList groceries={props.groceries.filter(grocery => grocery.quantity > 0)} updateQuantity={props.updateQuantity}/>
