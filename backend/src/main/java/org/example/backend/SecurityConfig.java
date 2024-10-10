b4be5d73fe20112248aeb422f57606dc8466b672
Nora Kauczor <n.kauczor@web.de>
2024-10-10 14:43:19 +0200

    correct value annotation in security config

--- backend/src/main/java/org/example/backend/SecurityConfig.java
+++ backend/src/main/java/org/example/backend/SecurityConfig.java
@@ -3,7 +3,6 @@ package org.example.backend;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,7 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Value("${APP_URL}")
+    @Value("${app.url}")
     private String appUrl;
 
     @Bean
