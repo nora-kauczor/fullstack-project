c77a9723f3f40539502f368a077143d04e43039e
sategie <sategie@yahoo.com>
2024-10-11 16:00:09 +0200

    Add GlobalExceptionHander using RestControllerAdvice

--- backend/src/main/java/org/example/backend/GlobalExceptionHandler.java
+++ backend/src/main/java/org/example/backend/GlobalExceptionHandler.java
@@ -1,4 +1,17 @@
 package org.example.backend;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ErrorMessage handleGlobalException(Exception e) {
+        ErrorMessage errorMessage = new ErrorMessage("An error has occurred" + e.getMessage());
+        return errorMessage;
+    }
+
 }
