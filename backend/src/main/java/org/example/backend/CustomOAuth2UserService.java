2f12e0b3c02852286d868c4a9035da45696a9e3d
stefansiebers <s3f3s-github@yahoo.com>
2024-10-11 18:45:59 +0200

    use authority from database for OAuth2User

--- backend/src/main/java/org/example/backend/CustomOAuth2UserService.java
+++ backend/src/main/java/org/example/backend/CustomOAuth2UserService.java
@@ -1,6 +1,7 @@
 package org.example.backend;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -8,7 +9,7 @@ import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -31,7 +32,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
                     return userRepo.save(newUser);
                 }
         );
-        return new DefaultOAuth2User(Collections.emptyList(),
+        return new DefaultOAuth2User(List.of(new SimpleGrantedAuthority(gitHubUser.authority())),
                 user.getAttributes(), "id");
     }
 }
